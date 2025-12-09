56206e2463f5dd1aac122cc4328b76b275e07fa5
D1_89388_Rushikesh <rushikeshpatil23052002@gmail.com>
2025-12-09 21:57:57 +0530

    Added skills : Nestjs & PostgreSql

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,7 +1,7 @@
 const skills = {
   frontend: ["React.js", "HTML", "CSS", "Bootstrap", "Tailwind CSS", "Vite"],
-  backend: ["Java", "Spring Boot", "Node.js", "Express", "C#/.NET"],
-  databases: ["MySQL", "MongoDB"],
+  backend: ["Java", "Spring Boot", "Node.js", "Express", "C#/.NET", "NestJS"],
+  databases: ["MySQL", "MongoDB", "PostgreSQL"],
   tools: ["Git", "Redux Toolkit", "Postman", "Vercel", "Render"],
   other: ["Algorithms", "Microservices", "Spring Security"]
 };
